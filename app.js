b3beaa5783b1120b67f601323983d103af6a2aee
olexii <omedvedew@ukr.net>
2021-01-14 00:33:31 +0200

    added slider

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    //   setting-name: setting-value
+    // autoplay: true,
+    autoplaySpeed: 10000,
+    arrows: false,
+    dots: true,
+    });
+});
