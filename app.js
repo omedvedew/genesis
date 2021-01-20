9cb7f3cac83a291e3a0347505813e5ae2bca1819
olexii <omedvedew@ukr.net>
2021-01-20 23:24:21 +0200

    added about-container

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
+
+// SLIDER SCRIPT
 $(document).ready(function(){
     $('.slider').slick({
     //   setting-name: setting-value
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 10000,
     arrows: false,
     dots: true,
     });
 });
+
+
+// ABOUT SCRIPT
+let toBSBtn = document.querySelector(".to-big-size-btn");
+let aboutImage = document.querySelector(".about-image");
+let aboutImageContainer = document.querySelector(".about-image-container");
+function changeImageBtn () {
+    if (aboutImage.classList.contains("about-image-active")) {
+        toBSBtn.classList.add("to-close-btn");
+    } else {
+        toBSBtn.classList.remove("to-close-btn");
+    };
+};
+function changeImage () {
+    aboutImage.classList.toggle("about-image-active");
+    aboutImageContainer.classList.toggle("about-image-container-active");
+}
+
+toBSBtn.addEventListener("click", function() {
+    changeImage();
+    changeImageBtn();
+});
+
+aboutImageContainer.addEventListener("click", function(e) {
+    if (e.target === aboutImageContainer) {
+        changeImage();
+    };
+    changeImageBtn();
+});
+
